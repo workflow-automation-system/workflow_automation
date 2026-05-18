808a816dd120f1d641fb7cb617299f552bd1c103
Oumayma <oumaymamaili@gmail.com>
2026-05-18 13:48:49 +0100

    .

--- workflow-automation-backend/auth-service/src/main/java/com/workflow_automation/auth_service/AuthServiceApplication.java
+++ workflow-automation-backend/auth-service/src/main/java/com/workflow_automation/auth_service/AuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.workflow_automation.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class AuthServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
--- workflow-automation-backend/auth-service/src/main/java/com/workflow_automation/auth_service/service/EmailService.java
+++ workflow-automation-backend/auth-service/src/main/java/com/workflow_automation/auth_service/service/EmailService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +16,7 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String fromEmail;
 
+    @Async
     public void sendVerificationEmail(String to, String verificationLink) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(fromEmail);
