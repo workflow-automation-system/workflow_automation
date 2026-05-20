931739190f0da7137463cbfe2340c6b424475cc3
Oumayma <oumaymamaili@gmail.com>
2026-05-20 22:08:39 +0100

    fixed auth service error

--- workflow-automation-backend/auth-service/src/test/java/com/workflow_automation/auth_service/AuthServiceApplicationTests.java
+++ workflow-automation-backend/auth-service/src/test/java/com/workflow_automation/auth_service/AuthServiceApplicationTests.java
@@ -2,12 +2,10 @@ package com.workflow_automation.auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.workflow_automation.auth_service.service.EmailService;
-import com.workflow_automation.auth_service.service.OrganizationClient;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import com.workflow_automation.auth_service.service.EmailService;
+import com.workflow_automation.auth_service.service.OrganizationClient;
 
 @SpringBootTest
 @ActiveProfiles("test")
