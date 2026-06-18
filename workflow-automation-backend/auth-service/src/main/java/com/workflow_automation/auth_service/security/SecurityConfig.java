b75946ff0b4fcca27053724be92f585674383308
Oumayma <oumaymamaili@gmail.com>
2026-06-18 16:16:53 +0100

    .

--- workflow-automation-backend/auth-service/src/main/java/com/workflow_automation/auth_service/security/SecurityConfig.java
+++ workflow-automation-backend/auth-service/src/main/java/com/workflow_automation/auth_service/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfig() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
